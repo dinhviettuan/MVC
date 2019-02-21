6bbd7e64aa0f5f6aaebf4296fbda6c33f69a422c
dinhviettuan <46431771+dinhviettuan@users.noreply.github.com>
2019-02-21 11:47:46 +0700

    Add file

---
+++ connect.php
@@ -0,0 +1,17 @@
+<?php
+$host = 'localhost';
+$db   = 'mylibary';
+$user = 'root';
+$pass = '';
+$options = [
+    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::ATTR_EMULATE_PREPARES   => false,
+];
+$dsn = "mysql:host=$host;dbname=$db;user=$user;pass=$pass;";
+try {
+     $pdo = new PDO($dsn, $user, $pass, $options);
+} catch (\PDOException $e) {
+     throw new \PDOException($e->getMessage(), (int)$e->getCode());
+}
+?> 
